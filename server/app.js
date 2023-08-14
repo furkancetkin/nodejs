b1c7be08c7e14e3814bd75154880ec7af0ed40ef
Furkan Çetkin <62360515+furkancetkin@users.noreply.github.com>
2023-08-14 22:03:29 +0300

    view engine

--- server/app.js
+++ server/app.js
@@ -5,20 +5,28 @@ const app = express();
 // listen for requests
 app.listen(3000);
 
+// register view engine
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        {title: 'Blog 1', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Blog 2', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Blog 3', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Blog 4', snippet: 'Lorem ipsum dolor sit amet consectetur'}
+    ]
+    res.render('index', { title: 'Home', blogs: blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' });
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create' });
+})
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: '404' });
 });
